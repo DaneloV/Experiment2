8927022e4944868b8ef3ececac962c4beaa76faf
DaneloV <danelo.vertucio.eng@ust.edu.ph>
2019-07-07 13:20:19 +0800

    fifth

---
+++ Experiment2_Problem5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	cout << "This is a program that will output the next 20 Fibonacci numbers (after 0 and 1) in one line, separated by commas." << endl;
+	int n1 = 0, n2 = 1, n3, n4 = 2, n5 = 2 + 20;
+
+	cout << "\nFibonacci Numbers: \n" << endl;
+	cout << n1 << " , " << n2 << " , ";
+
+	for (n4; n4 < n5; n4++)
+	{
+		n3 = n1 + n2;
+		n1 = n2;
+		n2 = n3;
+		cout << n3 << " , ";
+	}
+
+getch ();
+return 0;
+}
